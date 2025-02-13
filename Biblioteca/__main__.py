897f075de3e10aefd37314ff1c19d664a113ab93
liangreyes12 <jhonwoodz675@gmail.com>
2025-02-13 13:10:36 -0500

    clase principal parcialmente terminada

--- Biblioteca/__main__.py
+++ Biblioteca/__main__.py
@@ -1,3 +1,31 @@
-from biblioteca import Libros
-from limpieza import limpiarPantalla
+from biblioteca import Biblioteca
+import funciones as fn
 
+biblio = Biblioteca()
+try:
+    while True:
+        print("*******SISTEMA GESTOR DE BIBLIOTECA******\n")
+        print(f"ESCOGE UNA OPCION:\n1. Registrar Libro\n2. Buscar Libro por Autor\n3. Buscar Libro por Nombre\n4. Buscar Libro por Genero\n5. Buscar Libro por ISBN\n6. Salir\n")
+        opcion = int(input("- "))
+        match opcion:
+            case 1:
+               
+                print(biblio.registrarLibro())
+            case 2:
+                
+                print(biblio.buscarPorNombre())
+            case 3:
+                
+                biblio.buscarPorAutor()
+            case 4:
+               
+                biblio.buscarPorGenero()
+            case 5:
+                
+                print(biblio.buscarPorISBN())
+            case _:
+                print(f"OPCION INVALIDA INTENTALO OTRA VEZ")
+except ValueError:
+    print("INGRESE DATO VALIDO")
+except Exception as e: 
+    print(f"ERROR {e}")
